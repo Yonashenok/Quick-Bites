5efb792b50bc04686e8a7d396acf1af15839339c
Yonashenok <yonashenok24@gmail.com>
2023-07-08 19:14:07 +0300

    feat: nav handler

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import office from './images/office.jpg';
 import slimming from './images/slimming.jpg';
 import vegetarian from './images/Vegetarian.jpg';
 import healthy from './images/healthy.jpg';
+import family from './images/family.jpg';
+import dishes from './images/dishes.png';
+import delivery from './images/express-delivery.png';
+import pan from './images/pan.png';
+import vegetable from './images/vegetable.png';
 
 // renderMeals();
 const closeBtn = document.querySelector('.close-btn');
@@ -13,7 +18,26 @@ const overly = document.querySelector('.overly');
 const headerLogo = document.querySelector('.logo');
 const homeOverly = document.querySelector('.land-overly');
 const cardImg = document.querySelector('.card-img');
+const aboutImg = document.querySelector('.about-header');
+const familyImg = document.querySelector('.card-img-about');
+
+const dishesImg = document.querySelector('.img-1');
+const deliveryImg = document.querySelector('.img-2');
+const panImg = document.querySelector('.img-3');
+const vegetableImg = document.querySelector('.img-4');
+
+familyImg.style = ` background-image : url(${family})`;
+dishesImg.style = ` background-image : url(${dishes})`;
+deliveryImg.style = ` background-image : url(${delivery})`;
+vegetableImg.style = ` background-image : url(${vegetable})`;
+panImg.style = ` background-image : url(${pan})`;
+
 cardImg.style = ` background-image : url(${healthy})`;
+aboutImg.style = ` background-image :linear-gradient(
+  to bottom,
+  rgba(3, 61, 25, 0.6),
+  rgba(3, 61, 25, 0.5)
+), url(${landBg})`;
 homeOverly.style = ` background-image :linear-gradient(
   to bottom,
   rgba(3, 61, 25, 0.6),
@@ -23,26 +47,17 @@ homeOverly.style = ` background-image :linear-gradient(
 const menuData = [
   {
     img: vegetarian,
-    info: 'Vegetarian Menu',
     ele: document.querySelector('.vegetarian'),
   },
   {
     img: slimming,
-    info: 'Slimming Menu',
     ele: document.querySelector('.slimming'),
   },
   {
     img: office,
-    info: 'Office Menu',
     ele: document.querySelector('.office'),
   },
 ];
-// const menuOption = document.querySelector('.vegetarian');
-// menuOption.style = ` background-image :linear-gradient(
-//   to bottom,
-//   rgba(255, 255, 255, 1) 30%,
-//   rgba(3, 61, 25, 0.5)
-// ), url(${landBg})`;
 
 menuData.forEach((menu) => {
   menu.ele.style = ` background-image :linear-gradient(
